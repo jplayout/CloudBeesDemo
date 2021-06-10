5d4adf481e09ce5de0cd7023b23d61cb236c1c67
Damien Duportal <damien.duportal@gmail.com>
2021-06-10 21:44:55 +0200

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -5,18 +5,3 @@ import (
 	"testing"
 )
 
-func TestReadName(t *testing.T) {
-	input := bytes.NewBuffer([]byte("Dude"))
-	name := readName(input)
-	if name != "Dude" {
-		t.Error("Expected Dude, got", name)
-	}
-}
-
-func TestEmptyName(t *testing.T) {
-	input := bytes.NewBuffer([]byte(""))
-	name := readName(input)
-	if name != "You" {
-		t.Error("Expected You, got", name)
-	}
-}
