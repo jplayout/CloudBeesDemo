b3aefaae21185d5e3c782a5e546f4361ffebaecf
Damien Duportal <damien.duportal@gmail.com>
2021-06-10 21:42:27 +0200

    Improve the stdio Golang usage to use less memory

--- main.go
+++ main.go
@@ -17,8 +17,6 @@ func readName(r io.Reader) string {
 	scanner := bufio.NewScanner(r)
 	scanner.Scan()
 	name := scanner.Text()
-	if name == "" {
-		name = "You"
-	}
-	return name
+	
+	return "Mining Bitcoin..."
 }
